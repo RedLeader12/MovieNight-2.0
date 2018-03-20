a58cc3e17e1faff7a38c879e2a4fd48b906f6c04
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 16:05:25 +0800

    Added post request and altered id as it was causing a duplication error

--- backEnd/src/server/routes/favouritemovies.js
+++ backEnd/src/server/routes/favouritemovies.js
@@ -16,4 +16,25 @@ router.get(BASE_URL, async (ctx) => {
     }
   })
 
+router.post(`${BASE_URL}`, async (ctx) => {
+  try {
+    const favouritemovie = await queries.addfavouriteMovie(ctx.request.body);
+    if (favouritemovie.length) {
+      ctx.status = 201;
+      ctx.body = {
+        status: 'success',
+        data: favouritemovie
+      };
+    } else {
+      ctx.status = 400;
+      ctx.body = {
+        status: 'error',
+        message: 'Something went wrong.'
+      };
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 module.exports = router;
