355ecd3dea80747a461313cde882c9cc47d53de0
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 15:13:30 +0800

    Made a get request for the favouitemovies

--- backEnd/src/server/routes/favouritemovies.js
+++ backEnd/src/server/routes/favouritemovies.js
@@ -4,4 +4,16 @@ const queries = require('../db/queries/favouritemovies');
 const router = new Router();
 const BASE_URL = `/api/v1/favouritemovies`;
 
+router.get(BASE_URL, async (ctx) => {
+    try {
+      const favouritemovies = await queries.getAllfavouriteMovies();
+      ctx.body = {
+        status: 'success',
+        data: favouritemovies
+      };
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
 module.exports = router;
