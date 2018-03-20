6253ef6ac95b01f4be4afc7859629fff2fd44352
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 15:06:13 +0800

    Add test for get request to get all favourite movies from the database

--- backEnd/test/routes.favouritemovies.test.js
+++ backEnd/test/routes.favouritemovies.test.js
@@ -8,12 +8,30 @@ chai.use(chaiHttp);
 const server = require('../src/server/index');
 const knex = require('../src/server/db/connection');
 
-describe('routes : movies', () => {
+describe('routes : favouritemovies', () => {
 
   beforeEach(() => {
     return knex.migrate.rollback()
     .then(() => { return knex.migrate.latest(); })
     .then(() => { return knex.seed.run(); });
+  });
+
+  describe('GET /api/v1/favouritemovies', () => {
+    it('should return all favourite movies', (done) => {
+      chai.request(server)
+      .get('/api/v1/favouritemovies')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.length.should.eql(1);
+        res.body.data[0].should.include.keys(
+          '_id', 'overview', 'poster_path', 'release_date', 'title','vote_average','popularity',
+        );
+        done();
+      });
+    });
   });
 
   afterEach(() => {
