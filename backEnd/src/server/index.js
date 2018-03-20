daee2fb93feb541c5919bb51beae09ac1b6925b7
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 14:55:30 +0800

    Added favourite movies route to the port section

--- backEnd/src/server/index.js
+++ backEnd/src/server/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const indexRoutes = require('./routes/index');
+const movieRoutes = require('./routes/favouritemovies');
 
 const app = new Koa();
 const PORT = process.env.PORT || 3002;
 
 app.use(indexRoutes.routes());
+app.use(movieRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
