34ebcbf57f9b27e8e6ef3eb88a5730d33742c80f
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 10:45:27 +0800

    Add seed databse to check whether database can be filled

---
+++ backEnd/src/server/db/seeds/favouritesmovies_seed.js
@@ -0,0 +1,15 @@
+exports.seed = (knex, Promise) => {
+  return knex('favouritemovies').del()
+  .then(() => {
+    return knex('favouritemovies').insert({
+      _id: "396371",
+      overview: 'The Haunted House',
+      poster_path: "/h9hUP5ZJGsjL2wbERrGlj4dMjZq.jpg",
+      release_date: "2017-12-21",
+      title: "Top Gun",
+      vote_average: 5.0,
+      popularity: 9.9
+    });
+  })
+};
+
