013d1acc0548e2b21b6e94a8a0e98e0e864a5ec0
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 16:04:12 +0800

    Add post request for favouritemovies

--- backEnd/src/server/db/queries/favouritemovies.js
+++ backEnd/src/server/db/queries/favouritemovies.js
@@ -4,7 +4,14 @@ function getAllfavouriteMovies() {
     return knex('favouritemovies')
     .select('*');
   }
+
+function addfavouriteMovie(movie) {
+  return knex('favouritemovies')
+  .insert(movie)
+  .returning('*');
+}
   
   module.exports = {
-    getAllfavouriteMovies
+    getAllfavouriteMovies,
+    addfavouriteMovie
   };
