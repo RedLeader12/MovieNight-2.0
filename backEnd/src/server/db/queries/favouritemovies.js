f97a79956e7c7f1fbb9a26c1e0dac293bea0f2a4
RedLeader12 <redleader@jigsaw.xyz>
2018-03-20 14:57:39 +0800

    made a connection between the favouritemovies routes and database

--- backEnd/src/server/db/queries/favouritemovies.js
+++ backEnd/src/server/db/queries/favouritemovies.js
@@ -1,1 +1,9 @@
 const knex = require('../connection');
+
+const Router = require('koa-router');
+const queries = require('../db/queries/favouritemovies');
+
+const router = new Router();
+const BASE_URL = `/api/v1/favouritemovies`;
+
+module.exports = router;
