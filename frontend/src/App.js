c8bb1c2cd0f39489ee6f3602455eccfda49e797a
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 12:02:21 +0800

    Initialised browser router so react router can be used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import MovieList from './containers/MovieList'
+import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App"> 
-        < MovieList /> 
-      </div>
-    );
-  }
-}
+import MovieList from './containers/MovieList'
+import Favourites from './containers/Favourites'
+
+const App = () => (
+  <div className='App'>
+    <Switch>
+      <Route exact path='/' component={MovieList}/>
+      <Route exact path='/favourites' component={Favourites}/>
+    </Switch>
+  </div>
+)
 
 export default App;
