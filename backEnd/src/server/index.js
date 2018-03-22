6067be6fd309199968a0ddaeaee725d55ff7aaf1
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 17:29:51 +0800

    nothing changed

--- backEnd/src/server/index.js
+++ backEnd/src/server/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 const indexRoutes = require('./routes/index');
 const movieRoutes = require('./routes/favouritemovies');
@@ -8,6 +9,8 @@ const app = new Koa();
 const PORT = process.env.PORT || 3002;
 
 app.use(bodyParser());
+
+app.use(cors());
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
 
