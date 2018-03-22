c231d6b42c9d501bdd6df6d228b707bd572b68b1
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 10:51:44 +0800

    Added component willMoutn

--- frontend/src/containers/MovieList.js
+++ frontend/src/containers/MovieList.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import config from '../config.json'
 
 class MovieList extends Component {
-    constructor(props) {
-       super(props)
-       this.state = {
-         moviesList: [],
-        }
+constructor(props) {
+  super(props)
+    this.state = {
+      moviesList: [],
     }
+}
+
+componentDidMount() {
+  let self=this;
+
+    window.fetch(config.api)
+        .then(function(results) {
+        return results.json();
+        })
+        .then(function(data){
+
+            console.log(data.results)
+            self.setState({
+            moviesList: data.results
+            })
+        })
+        .catch(function(error) {
+        console.log(error)
+        });
+}
 
 render() {
     return (
