165666270c1697b1b96191f1ddea766c38fb208b
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 14:36:52 +0800

    Changed Favourite to FavouritesList in Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import MovieList from './containers/MovieList'
-import Favourites from './containers/Favourites'
+import FavouritesList from './containers/FavouritesList'
 
 const App = () => (
   <div className='App'>
     <Switch>
       <Route exact path='/' component={MovieList}/>
-      <Route exact path='/favourites' component={Favourites}/>
+      <Route exact path='/favourites' component={FavouritesList}/>
     </Switch>
   </div>
 )
