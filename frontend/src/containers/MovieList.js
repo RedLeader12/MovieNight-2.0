8c61a8a74f6738f92c32962d869934d7b98280c6
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 11:11:29 +0800

    Printed out movies from Api

--- frontend/src/containers/MovieList.js
+++ frontend/src/containers/MovieList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import config from '../config.json'
 
+import Movie from '../components/Movie.js'
+
 class MovieList extends Component {
-constructor(props) {
-  super(props)
-    this.state = {
-      moviesList: [],
+    constructor(props) {
+    super(props)
+        this.state = {
+        moviesList: [],
+        }
     }
-}
 
 componentDidMount() {
   let self=this;
@@ -17,7 +19,6 @@ componentDidMount() {
         return results.json();
         })
         .then(function(data){
-
             console.log(data.results)
             self.setState({
             moviesList: data.results
@@ -29,9 +30,21 @@ componentDidMount() {
 }
 
 render() {
+
+    let movies = this.state.moviesList.map((movie, index) =>(
+        <Movie
+        key = {index}
+        title={movie.title}
+        overview={movie.overview}
+        voteAverage={movie.vote_average}
+        popularity={movie.popularity}
+        posterPath={"https://image.tmdb.org/t/p/w200"+ movie.poster_path}
+        /> 
+    ))
+
     return (
         <div className="MovieList">
-
+            {movies}
         </div>
     );
   }
