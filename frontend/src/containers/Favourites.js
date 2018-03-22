e68af72c5be3412cc922d83ccda92f33d53e0831
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 14:30:21 +0800

    Got favourites to print out data from the api. Gosh

--- frontend/src/containers/Favourites.js
+++ frontend/src/containers/Favourites.js
@@ -12,18 +12,42 @@ class Favourites extends Component {
         }
     }
 
-componentWillMount() {
-   axios.get(config.database)
-   .then(response => {this.setState({favourites: response.data.data})}
-)}
+componentDidMount() {
+    let self=this;
+
+    window.fetch(config.database)
+        .then(function(results) {
+        return results.json();
+        })
+        .then(function(data){
+            console.log(data.data)
+            self.setState({
+            favourites: data.data
+            })
+        })
+        .catch(function(error) {
+        console.log(error)
+        });
+//    axios.get(config.database)
+//    .then(res => {this.setState({favourites: res.data.data})})
+//    .catch(error => console.log(error));
+}
 
 render() {
 
-    let favourites = this.state.favourites
+    let favourites = this.state.favourites.map((movie, index) =>(
+        <Movie
+        key = {index}
+        title={movie.title}
+        posterPath={"https://image.tmdb.org/t/p/w200"+ movie.poster_path}
+        /> 
+    ))
+
     console.log(favourites)
 
     return (
         <div className="Favourites">
+        {favourites}
         </div>
     );
   }
