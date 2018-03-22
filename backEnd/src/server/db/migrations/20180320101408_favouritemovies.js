cdd642072fb618a439c62562aa1e7fa34941c16c
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 17:29:11 +0800

    Readded popularity data, nothing changed

--- backEnd/src/server/db/migrations/20180320101408_favouritemovies.js
+++ backEnd/src/server/db/migrations/20180320101408_favouritemovies.js
@@ -1,11 +1,11 @@
 exports.up = (knex, Promise) => {
     return knex.schema.createTable('favouritemovies', (table) => {
       table.increments();
-      table.string('overview').notNullable();
+      table.text('overview').notNullable();
       table.string('poster_path').notNullable();
       table.string('release_date').notNullable();
       table.string('title').notNullable();
-      table.integer('vote_average').notNullable();
+      table.decimal('vote_average').notNullable();
       table.decimal('popularity').notNullable();
     });
   };
