ec425ef0c75bf1b9b05d6ab1331986bdf711633b
RedLeader12 <redleader@jigsaw.xyz>
2018-03-22 09:23:55 +0800

    Remove condition of only allowing five in a database. Not sure how to do this yet

--- backEnd/src/server/db/seeds/favouritesmovies_seed.js
+++ backEnd/src/server/db/seeds/favouritesmovies_seed.js
@@ -10,5 +10,35 @@ exports.seed = (knex, Promise) => {
       popularity: 9.9
     });
   })
+.then(() => {
+  return knex('favouritemovies').insert({
+    overview: 'The Death Cure KILLS. Find out more',
+    poster_path: "/h9hUP5ZJGsjL2wbERrG34543.jpg",
+    release_date: "2017-09-21",
+    title: "The Death Cure",
+    vote_average: 8.0,
+    popularity: 5.9
+  });
+})
+.then(() => {
+  return knex('favouritemovies').insert({
+    overview: 'Poppy playtime is not the right playtime',
+    poster_path: "/h9hUP5ZJGsjL2wbERrGlj4dMjZq.jpg",
+    release_date: "2017-04-21",
+    title: "Playtime",
+    vote_average: 10.0,
+    popularity: 10 
+  });
+})
+.then(() => {
+  return knex('favouritemovies').insert({
+    overview: 'Postman Pat, the legend',
+    poster_path: "/h9hUP5ZJGsjL2wbERrGlj4dMjZq.jpg",
+    release_date: "2017-02-21",
+    title: "PostmAN pAT",
+    vote_average: 5.0,
+    popularity: 6.4
+  });
+})
 };
 
