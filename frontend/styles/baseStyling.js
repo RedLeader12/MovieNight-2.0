a876e7016f409c4b19dfe0146fc04793e008af96
RedLeader12 <redleader@jigsaw.xyz>
2018-03-23 14:27:16 +0800

    Add common styling in styles folder to be references to

---
+++ frontend/styles/baseStyling.js
@@ -0,0 +1,23 @@
+const COLOURS = {
+    primary: "#1f2633",
+    secondary: "#161c24",
+    third: "#14171f",
+    dark: "#020203",
+    light: '363d4b',
+    white: "#eeeeef",
+    yellow: "ecc044",
+  }
+
+  module.exports = {
+   colours: COLOURS, 
+   headerfont: {
+    fontFamily: 'Raleway',
+    fontWeight: '700',
+    src: url('https://fonts.googleapis.com/css?family=Raleway:600,700')
+   },
+   font: {
+    fontFamily: 'Raleway',
+    fontWeight: '600',
+    src: url('https://fonts.googleapis.com/css?family=Raleway:600,700')  
+   }
+  }
