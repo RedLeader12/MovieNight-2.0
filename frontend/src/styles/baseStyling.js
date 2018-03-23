e12414699b4a761d9ea448b8fce14c47ae643e47
RedLeader12 <redleader@jigsaw.xyz>
2018-03-23 15:52:54 +0800

    Setup base styling for common styles

---
+++ frontend/src/styles/baseStyling.js
@@ -0,0 +1,16 @@
+
+
+const COLOURS = {
+    primary: "#1f2633",
+    secondary: "#161c24",
+    third: "#14171f",
+    dark: "#020203",
+    light: '363d4b',
+    white: "#eeeeef",
+    yellow: "ecc044",
+  }
+
+
+  module.exports = {
+   colours: COLOURS
+  }
