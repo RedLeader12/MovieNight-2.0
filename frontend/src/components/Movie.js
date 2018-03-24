6fa75228a7732d3f58c990411be6e7d7cebbfe98
RedLeader12 <redleader@jigsaw.xyz>
2018-03-24 09:43:58 +0800

    Did foundation of image positioning in the table

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -6,11 +6,12 @@ class Movie extends Component {
   render () {
     return (
         <div className="Movie" style={localStyles.Movie}>
-            <img id="posterPath" src={this.props.posterPath} style={localStyles.posterPath}/>
+            <img id="posterPath" style={localStyles.poster} src={this.props.posterPath}/>
               <h2 id="title" >{this.props.title}</h2>
               <span>{this.props.voteAverage} </span>
               <span id="popularity">{this.props.popularity} </span>
               <span id="overview" style={localStyles.overview}>{this.props.overview} </span>
+              < br/> 
               <button onClick={this.props.onClick}> Favour Movie </button> 
         </div>
     );
@@ -23,6 +24,10 @@ const localStyles = {
     flex: 2, 
     border: '3px solid green',
     position: 'relative',
-    padding: 20,
+    padding: 22,
   },
+  poster: {
+    borderRadius: 20, 
+    boxShadow: '0px 10px 5px' + Styles.colours.third,
+  }
 }
