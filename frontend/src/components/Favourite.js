63d77cdc255544721d8ac85efdf944edfd1cf80d
RedLeader12 <redleader@jigsaw.xyz>
2018-03-26 17:16:20 +0800

    Styled basic of favourites

--- frontend/src/components/Favourite.js
+++ frontend/src/components/Favourite.js
@@ -6,9 +6,11 @@ class Favourite extends Component {
   render () {
     return (
         <div className="FavouritesList" style={localStyles.FavouritesList}>
-            <img src={this.props.posterPath} /> 
-            <span id="title"> {this.props.title} </span>
-            <button onClick={this.props.onClick}> Delete Movie </button> 
+            <img style={localStyles.image} src={this.props.posterPath} /> 
+            <div style={localStyles.detailBox}> 
+            <span style={localStyles.title} id="title"> {this.props.title} </span>
+            <button onClick={this.props.onClick} style={localStyles.button}> Delete Movie </button> 
+            </div> 
         </div>
     );
   }
@@ -20,8 +22,33 @@ const localStyles = {
     border: '3px solid green',
     width: '15%',
     position: 'relative',
-    left: '10.5%',
-    marginTop: '11%',
-    marginBottom: '10.6%'
+    left: 45,
+    marginTop: '10%',
+    marginBottom: '10.6%',
+    padding: 20
+  },
+  detailBox: {
+    marginTop: 15
+  },
+  image: {
+    borderRadius: 20, 
+    boxShadow: '0px 10px 5px' + Styles.colours.third,
+  },
+  title: {
+    fontSize: 15,
+    color: Styles.colours.white 
+  },
+  button: {
+    outline: 'none',
+    padding: 10, 
+    fontSize: 12,
+    borderRadius: 10, 
+    width: '60%',
+    color: Styles.colours.white, 
+    fontWeight: '600', 
+    background: 'linear-gradient(to left,'+ Styles.colours.yellow + ','  + Styles.colours.darkyellow + ')',
+    marginLeft: 45,
+    marginTop: 20,
+    border: 'none',
   }
 }
