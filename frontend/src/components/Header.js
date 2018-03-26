c4881cd6228d07981704aa8b7aed4cc55a4f8b1e
RedLeader12 <redleader@jigsaw.xyz>
2018-03-26 16:45:44 +0800

    Add favourites button for Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import Styles from '../styles/baseStyling'
+import Styles from '../styles/baseStyling';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render () {
     return (
         <div className="Header" style={localStyles.Header}>
             <span style={localStyles.title}> {this.props.header} </span> 
+            <Link to='/favourites'><button onClick={this.props.onClick} style={localStyles.button}> Favourites </button></Link>  
         </div>
     );
   }
@@ -15,10 +17,10 @@ export default Header;
 
 const localStyles = { 
   Header: {
-    border: '3px solid blue',
     height: '80px',
     position: 'relative',
     zIndex: 1000,
+    border: '3px solid blue'
   },
   title: {
     color: Styles.colours.white,
@@ -28,5 +30,18 @@ const localStyles = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)'
+  },
+  button: {
+    outline: 'none',
+    padding: 10, 
+    fontSize: 12,
+    borderRadius: 10, 
+    width: '8%',
+    color: Styles.colours.white, 
+    fontWeight: '600', 
+    background: 'linear-gradient(to left,'+ Styles.colours.yellow + ','  + Styles.colours.darkyellow + ')',
+    marginLeft: '89%',
+    marginTop: 20,
+    border: 'none',
   }
 }
