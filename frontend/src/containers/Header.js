e7854b12f5af6b1e924032d5fd0a1a2ae71430a3
RedLeader12 <redleader@jigsaw.xyz>
2018-03-27 21:10:48 +0800

    Header needs state so its a container now

---
+++ frontend/src/containers/Header.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import Styles from '../styles/baseStyling';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props)
+        this.state = {
+        search: ''
+        }
+    }
+
+searchInputHandler = (event) => {
+  this.setState({ search: event.target.value})
+  console.log(this.state.search)
+}
+
+  render () {
+    return (
+        <div className="Header" style={localStyles.Header}>
+            <span style={localStyles.title}> {this.props.header} </span> 
+            <Link to='/favourites'><button onClick={this.props.onClick} style={localStyles.button}> Favourites </button></Link> 
+            <Link to='/discover'><button onClick={this.props.onClick} style={localStyles.button}> Discover </button></Link> 
+            <span> {this.state.search} </span> 
+            <input type="text" name="search" onChange={this.searchInputHandler}/>
+        </div>
+    );
+  }
+}
+export default Header;
+
+const localStyles = { 
+  // Header: {
+  //   height: '80px',
+  //   position: 'relative',
+  //   zIndex: 1000,
+  // },
+  // title: {
+  //   color: Styles.colours.white,
+  //   fontWeight: '500',
+  //   fontSize: 30,
+  //   position: 'absolute',
+  //   top: '50%',
+  //   left: '50%',
+  //   transform: 'translate(-50%, -50%)'
+  // },
+  // button: {
+  //   outline: 'none',
+  //   padding: 10, 
+  //   fontSize: 13,
+  //   borderRadius: 10, 
+  //   width: '8%',
+  //   color: Styles.colours.white, 
+  //   fontWeight: '600', 
+  //   background: 'linear-gradient(to left,'+ Styles.colours.yellow + ','  + Styles.colours.darkyellow + ')',
+  //   marginLeft: '89%',
+  //   marginTop: 20,
+  //   border: 'none',
+  // }
+}
