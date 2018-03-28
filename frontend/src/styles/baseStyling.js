92a4a8937236909df45d1673776300c630955cb3
RedLeader12 <redleader@jigsaw.xyz>
2018-03-28 14:08:34 +0800

    Added custom button styling

--- frontend/src/styles/baseStyling.js
+++ frontend/src/styles/baseStyling.js
@@ -11,4 +11,31 @@ const COLOURS = {
 
   module.exports = {
    colours: COLOURS,
+   imageStyle: {
+    boxShadow: '0px 10px 5px #14171f',
+    borderRadius: 20,
+    marginLeft: 20
+   },
+   titleStyles: {
+     color: 'white', 
+     fontSize: 15 
+   },
+   button: {
+     backgroundColor: "#ecc044",
+     color: "#eeeeef",
+     padding: '10px 20px', 
+     borderRadius: 20, 
+     border: 'none', 
+     outline: 'none', 
+     fontWeight: '600',
+     marginLeft: '20%'
+   },
+   like: {
+    backgroundColor: "#ecc044",
+    borderRadius: 1000, 
+    padding: '8px 10px', 
+    fontSize: 14, 
+    color: 'white',
+    marginLeft: '5%'
+   }
   }
