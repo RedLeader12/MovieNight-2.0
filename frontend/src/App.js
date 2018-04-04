62295dd6542b66a2ec9e3468e9a5471b07d6d49b
RedLeader12 <redleader@jigsaw.xyz>
2018-04-04 19:16:11 +0800

    Reset code due to rerdering of component hierachy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import MovieList from './controllers/MovieList'
-import FavouritesList from './controllers/FavouritesList'
-import Header from './controllers/Header'
+import MovieController from './controllers/MovieController';
+import Styles from './styles/baseStyling';
 
-import Styles from './styles/baseStyling'
-
-const headingTitle = 'MovieNight ☾'
+const headingTitle = 'MovieNight ☾';
 
 const App = () => (
-  <div className='App' style={localStyles.App}>
-  <link href="https://fonts.googleapis.com/css?family=Quicksand:500:700" rel="stylesheet"/>
-  <Header header={headingTitle}/>
-    <Switch>
+  <div className="App" style={localStyles.App}>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:500:700" rel="stylesheet" />
+    <MovieController />
+    {/* <Switch>
       <Route exact path='/discover' component={MovieList}/>
       <Route exact path='/favourites' component={FavouritesList}/>
-    </Switch>
+    </Switch> */}
   </div>
-)
+);
 
 export default App;
 
-const localStyles = { 
+const localStyles = {
   App: {
-      fontFamily: 'QuickSand, sans-serif'
+    fontFamily: 'QuickSand, sans-serif'
   },
-}
+};
---
+++ frontend/src/controllers/MovieController.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import config from '../config.json';
+
+
+import Styles from '../styles/baseStyling';
+
+class MovieController extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      moviesList: [],
+      favouritesList: [],
+    };
+  }
+
+  componentDidMount() {
+    const self = this;
+
+    window.fetch(config.api)
+      .then(results => results.json())
+      .then((data) => {
+        console.log(data.results);
+        const final = data.results;
+        self.setState({
+          moviesList: final
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <p> hello </p>
+      </div>
+    );
+  }
+}
+
+export default MovieController;
+
+// const localStyles = {
+//     // searchBox: {
+//     //     width: 300,
+//     //     position: 'absolute',
+//     //     left: '80%',
+//     //     marginTop:  47
+//     // },
+//     // search: {
+//     //     width: 250,
+//     //     padding: 5,
+//     //     outline: 'none'
+//     // }
+
+//   }
